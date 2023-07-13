2d472570739b62a3104c2e76de2e8977d1bed072
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-07-13 14:10:05 +0200

    modified my file on my branch Eleonora

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const team= [];
+const team = [
+    {
+      name: "Eleonora",
+      surname: "Melodia",
+      age: 31,
+      city: "Palermo",
+      hobby: "Crossfit",
+      favoriteFood: "Pizza",
+      favoriteVideoGame: "Sims",
+      favoriteFilm: "Notte da leoni",
+      favoriteBook: "Piccolo principe",
+      petName: "Zelda",
+    },
+  ]
