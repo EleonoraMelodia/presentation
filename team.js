84d3325644d94aeacf566b2b5b2480f50013d5f4
AngeloGallina <angeloglav@gmail.com>
2023-07-13 13:48:12 +0200

    add info

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const team= [];
+const team= [
+      {
+    name: "Angelo",
+    surname: "Gallina",
+    age: 23,
+    city: "Caltanissetta",
+    hobby: "Calcio, Videogames",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "Elden Ring",
+    favoriteFilm: "The Wolf Of The WallStreet",
+    favoriteBook: "Insurgent",
+    petName: "Zody",
+      }
+];
