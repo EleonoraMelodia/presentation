a96f87406f107a8974b350f1f37071cb36de45ae
Miriana Falcone <miriana.falcone7@gmail.com>
2023-07-13 13:48:09 +0200

    Miriana

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const team= [];
+const team= [{
+      name: "Miriana",
+    surname: "Falcone",
+    age: 21,
+    city: "Manfredonia",
+    hobby: "Reading",
+    favoriteFood: "Sushi",
+    favoriteVideoGame: "",
+    favoriteFilm: "",
+    favoriteBook: "Le affinità elettive",
+    petName: "",
+}];
