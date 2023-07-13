0547b51e0be38ad8f16aa224d5f16b555dd49d93
Luigi Rauso <darjkzer@hotmail.it>
2023-07-13 13:53:17 +0200

    added new info to team.js

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const team= [];
+const team = [
+    {
+      name: "Luigi",
+      surname: "Rauso",
+      age: 25,
+      city: "Napoli",
+      hobby: "Music",
+      favoriteFood: "Pasta",
+      favoriteVideoGame: "Valorant",
+      favoriteFilm: "The Legend of 1900",
+      favoriteBook: "Metro 2033",
+      petName: "Rudy",
+    },
+  ];
